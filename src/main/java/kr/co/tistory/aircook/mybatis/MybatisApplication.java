e366387981eec6d0ad6e450eb6d343c2fa17c9c4
francis.lee <aircook@gmail.com>
2020-11-09 19:49:55 +0900

    프로젝트 시작

---
+++ src/main/java/kr/co/tistory/aircook/mybatis/MybatisApplication.java
@@ -0,0 +1,13 @@
+package kr.co.tistory.aircook.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisApplication.class, args);
+    }
+
+}
